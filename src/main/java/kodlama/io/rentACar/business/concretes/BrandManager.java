da8e1d374bf9644e6d87f471db09864eb7092502
Alparslan <alparslanaydgn02@gmail.com>
2023-03-10 23:32:10 +0300

    Project codes added

---
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -0,0 +1,26 @@
+package kodlama.io.rentACar.business.concretes;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+@Service//Ioc'ye bu manageri ekliyor. Ve her defasında newlemek yerinde 1 tane newliyor ve onu veriyor
+public class BrandManager implements BrandService {
+
+	private BrandRepository brandRepository;
+
+	public BrandManager(BrandRepository brandRepository) {
+		this.brandRepository = brandRepository;
+	}
+
+	@Override
+	public List<Brand> getAll() {
+		return brandRepository.getAll();
+		
+	}
+
+}
