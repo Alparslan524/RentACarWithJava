5e706142325e658a8c2a95fc9d328d93ffe07576
Alparslan <alparslanaydgn02@gmail.com>
2023-03-20 21:40:51 +0300

    Added Brand delete, update, getById operations

--- src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
 import kodlama.io.rentACar.core.utilities.mappers.abstracts.ModelMapperService;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
@@ -21,7 +23,7 @@ public class BrandManager implements BrandService {
 
 //	public BrandManager(BrandRepository brandRepository) {
 //	this.brandRepository = brandRepository;
-//}//@AllArgsConstructor sayesinde bu satırı otomatik yaptık(Lombok ile)
+//}  @AllArgsConstructor sayesinde bu satırı otomatik yaptık(Lombok ile)
 
 	private BrandRepository brandRepository;
 	private ModelMapperService modelMapperService;
@@ -53,5 +55,23 @@ public class BrandManager implements BrandService {
 		Brand brand = modelMapperService.forRequest().map(createBrandRequest, Brand.class);
 
 		brandRepository.save(brand);
+	}
+
+	@Override
+	public GetByIdBrandResponse getById(int id) {
+		Brand brand = brandRepository.findById(id).orElseThrow();
+		GetByIdBrandResponse response = modelMapperService.forResponse().map(brand, GetByIdBrandResponse.class);
+		return response;
+	}
+
+	@Override
+	public void update(UpdateBrandRequest updateBrandRequest) {
+		Brand brand = modelMapperService.forRequest().map(updateBrandRequest, Brand.class);
+		brandRepository.save(brand);
+	}
+
+	@Override
+	public void delete(int id) {
+		brandRepository.deleteById(id);
 	}
 }
